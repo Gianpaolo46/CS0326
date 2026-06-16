db4a75cbbce9c124cce44968178d1badbf151bad
Gianpaolo46 <scagionegianpaolo@gmail.com>
2026-06-16 17:04:34 +0200

    Consegna Pratica S2/L2

---
+++ UNIT_1/S2/L2/Consegna Pratica S2-L2.py
@@ -0,0 +1,13 @@
+# Esercizio Pratica S2/L2
+
+# Scrivere un programma in Python che genera un nome per una band musicale 
+# utilizzando due input forniti dall'utente: 
+# la città di origine e il nome del proprio animale domestico.
+
+città = input("Nome città: ")
+pet = input("Nome animale: ")
+
+nome_band = città + pet
+
+print("Nome band generato:", nome_band)
+
