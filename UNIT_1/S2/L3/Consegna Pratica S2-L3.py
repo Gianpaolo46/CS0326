7679719e8be321e07f0e292e41559ddb9c9a3ab9
Gianpaolo46 <scagionegianpaolo@gmail.com>
2026-06-17 17:12:29 +0200

    Consegna Pratica S2/L3

---
+++ UNIT_1/S2/L3/Consegna Pratica S2-L3.py
@@ -0,0 +1,36 @@
+
+figure = input("scegli una figura geometrica tra quadrato, cerchio o rettangolo: ")
+
+while True:
+    if figure == "quadrato":
+        lato = float(input("inserisci la lunghezza del lato del quadrato: "))
+        perimetro = 4 * lato
+        print("il perimetro del quadrato è: " + str(perimetro))
+
+    elif figure == "cerchio":
+        raggio = float(input("inserisci il raggio del cerchio: "))
+        circonferenza = 2 * 3.14 * raggio
+        print("la circonferenza del cerchio è: " + str(circonferenza))
+
+    elif figure == "rettangolo":
+        base = float(input("inserisci la base del rettangolo: "))
+        altezza = float(input("inserisci l'altezza del rettangolo: "))
+        perimetro = 2 * (base + altezza)
+        print("il perimetro del rettangolo è: " + str(perimetro))
+
+    else: print("figura geometrica non valida")
+
+    while True:
+        risposta = input("vuoi scegliere un'altra figura? rispondi con sì o no: ")
+
+        if risposta == "sì":
+            figure = input("scegli una figura geometrica tra quadrato, cerchio o rettangolo: ")
+            break
+
+        elif risposta == "no": break
+
+        else: print("risposta non valida")
+
+    if risposta == "no": break
+
+
